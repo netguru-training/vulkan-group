6eba58574b29aa0fbf150389ff0dc452a5dcbf9c
Grzegorz Flejszar <flejszar@gmail.com>
2015-06-27 13:01:46 +0200

    Add total price and total discount.

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,4 +8,12 @@ class Order < ActiveRecord::Base
             length: { maximum: 50 },
             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+[a-z]+\z/i },
             uniqueness: { case_sensitive: false }
+
+  def total_price
+    self.products.sum(:price) if self.products.count != 0
+  end
+
+  def total_discount(discount_percentage)
+    total_price * (discount_percentage / 100) unless total_price.nil?
+  end
 end
