6a8f708b4bbd7aba9e2fa33e82a60bfc7c18f8d6
herr-zimt <derzimt@gmail.com>
2015-06-27 15:44:59 +0200

    fix product model | rubocop

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,18 +5,19 @@ class Product < ActiveRecord::Base
   validates_presence_of :name, :description, :price, :stock
 
   validates_numericality_of :price, greater_than_or_equal_to: 0.01
-  validates_numericality_of :stock, only_integer: true,
-    greater_than_or_equal_to: 0
+  validates_numericality_of :stock,
+                            only_integer: true,
+                            greater_than_or_equal_to: 0
 
   validates_format_of :price, with: /\A\d+(?:\.\d{0,2})?\z/
 
   validate :image_size
 
-  private 
+  private
+
     def image_size
       upload_limit = 1
-      if image.size > upload_limit.megabyte
-        errors.add(:image, "You cannot upload a file greater than #{upload_limit}MB")
-      end
+      msg = "You cannot upload a file greater than #{upload_limit}MB"
+      errors.add(:image, msg) if image.size > upload_limit.megabyte
     end
 end
