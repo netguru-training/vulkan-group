829dd40714ad304318b3398d68f96e20adcdf413
herr-zimt <derzimt@gmail.com>
2015-06-27 12:15:13 +0200

    add categories product reference

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627090142) do
+ActiveRecord::Schema.define(version: 20150627101154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20150627090142) do
     t.integer  "stock",                               null: false
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "category_id"
   end
 
   create_table "users", force: :cascade do |t|
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe Product, :type => :model do
   it { should validate_presence_of(:price) }
   it { should validate_presence_of(:description) }
   it { should validate_presence_of(:stock) }
-  # it { should belong_to(:category) }
+
   it { should validate_numericality_of(:price) }
   it { should validate_numericality_of(:stock) }
+
+  it { should belong_to(:category) }
 
 end
