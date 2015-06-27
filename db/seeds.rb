d09df2aa632a5392f72da48a937d4a5d67d9c4a3
Adam Winiarczyk <daktyr@gmail.com>
2015-06-27 12:50:04 +0200

    User, admin, category seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#Users
+User.create(name: 'Maria', email: 'maria@example.com', password: 'password', password_confirmation: 'password')
+User.create(name: 'Dominik', email: 'dominik@example.com', password: 'password', password_confirmation: 'password')
+User.create(name: 'Justyna', email: 'justyna@example.com', password: 'password', password_confirmation: 'password')
+User.create(name: 'Grzegorz', email: 'grzegorz@example.com', password: 'password', password_confirmation: 'password')
+User.create(name: 'Adam', email: 'adam@example.com', password: 'password', password_confirmation: 'password')
+
+#Admin user
+AdminUser.create(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+#Categories
+Category.find_or_create_by(name: 'Vegetables')
+Category.find_or_create_by(name: 'Fruits')
+Category.find_or_create_by(name: 'Fast food')
