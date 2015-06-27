6d52513465cec350f8c8a817a2303bc2f4b96f32
Justyna Rachowicz <justynarachowicz@gmail.com>
2015-06-27 15:22:41 +0200

    Add products controller and views

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,10 @@
+class ProductsController < ApplicationController
+  expose(:products)
+  expose(:product)
+  
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products, only: [:index, :show]
   root 'static_pages#index'
   ActiveAdmin.routes(self)
   devise_for :users
