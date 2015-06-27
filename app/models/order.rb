77b311f338475d572556d609a49dcd5e249f8c69
herr-zimt <derzimt@gmail.com>
2015-06-27 11:11:10 +0200

    add user-order relation, order validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,10 @@
 class Order < ActiveRecord::Base
   has_many :products
+  belongs_to :user
 
   validates_presence_of :address, :email
+
+  validates :email, length: { maximum: 50 },
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+[a-z]+\z/i },
+    uniqueness: { case_sensitive: false }
 end
