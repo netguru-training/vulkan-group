c4b6d5db1eef15a3460d456c28aa8084bd212902
Adam Winiarczyk <daktyr@gmail.com>
2015-06-27 14:00:26 +0200

    Add admin field migration to users. Add admin account to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,11 @@
 #Users
-User.create(name: 'Maria', email: 'maria@example.com', password: 'password', password_confirmation: 'password')
 User.create(name: 'Dominik', email: 'dominik@example.com', password: 'password', password_confirmation: 'password')
 User.create(name: 'Justyna', email: 'justyna@example.com', password: 'password', password_confirmation: 'password')
 User.create(name: 'Grzegorz', email: 'grzegorz@example.com', password: 'password', password_confirmation: 'password')
 User.create(name: 'Adam', email: 'adam@example.com', password: 'password', password_confirmation: 'password')
 
 #Admin user
-AdminUser.create(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+User.create(name: 'Maria', email: 'admin@example.com', password: 'password', password_confirmation: 'password', admin: true)
 
 #Categories
 vegetables = Category.find_or_create_by(name: 'Vegetables')
