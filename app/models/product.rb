ce434bb09124c1f8c6edcbb098cf0df7832ec2c2
herr-zimt <derzimt@gmail.com>
2015-06-27 10:55:00 +0200

    add product category relations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+  belongs_to :category
   validates_presence_of :name, :description, :price, :stock
 
   validates_numericality_of :price, greater_than_or_equal: 0.01
