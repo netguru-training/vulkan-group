55d345d154e43c456f02cf4dc36646063a53e610
herr-zimt <derzimt@gmail.com>
2015-06-27 11:53:03 +0200

    add spec for category and product

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe Product, :type => :model do
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:price) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:stock) }
+  # it { should belong_to(:category) }
+  it { should validate_numericality_of(:price) }
+  it { should validate_numericality_of(:stock) }
+
+end
