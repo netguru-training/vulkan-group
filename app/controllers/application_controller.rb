ed2ceedeb76079550ef7054fb93e54efdbe7439b
Adam Winiarczyk <daktyr@gmail.com>
2015-06-27 16:46:35 +0200

    Change find_categories to helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :find_categories
-
-
   def find_categories
     @categories = Category.all
   end
@@ -18,4 +15,6 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
+
+  helper_method :find_categories
 end
