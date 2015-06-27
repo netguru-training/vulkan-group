2c17c63ad632223dc234479c2ff18760574acef1
herr-zimt <derzimt@gmail.com>
2015-06-27 13:01:22 +0200

    fix product spec according to rubocop

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,6 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe Product, :type => :model do
-
+RSpec.describe Product, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:price) }
   it { should validate_presence_of(:description) }
@@ -12,18 +11,19 @@ RSpec.describe Product, :type => :model do
 
   it { should belong_to(:category) }
 
-  describe '#price' do
-    let(:product) { Product.new(
-      name: "Glass",
-      description: "Test description",
-      stock: 2
-    )}
+  describe 'price' do
+    let(:product) {
+      Product.new(
+        name: 'Glass',
+        description: 'Test description',
+        stock: 2
+      )}
 
     it 'is limited to two decimal places' do
       product.price = 9.999
       expect(product.save).to be_falsey
     end
- 
+
     it 'is greater than or equal to 0.01' do
       product.price = 5
       expect(product.save).to be_truthy
@@ -33,15 +33,15 @@ RSpec.describe Product, :type => :model do
       product.price = -55
       expect(product.save).to be_falsey
     end
-
   end
 
-  describe '#stock' do
-    let(:product) { Product.new(
-      name: "Glass",
-      description: "Test description",
-      price: 11.11,
-    )}
+  describe 'stock' do
+    let(:product) {
+      Product.new(
+        name: 'Glass',
+        description: 'Test description',
+        price: 11.11
+      )}
 
     it 'is limited to positive integer value' do
       product.stock = -1
@@ -56,7 +56,5 @@ RSpec.describe Product, :type => :model do
       product.stock = 2
       expect(product.save).to be_truthy
     end
-
   end
-
 end
