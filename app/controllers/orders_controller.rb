bd8e614b05406115d21c4d5eb714fd6c4e707461
herr-zimt <derzimt@gmail.com>
2015-06-28 16:23:58 +0200

    cart cleanup after order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,10 +2,9 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   expose(:order, attributes: :order_params)
-  expose(:cart)
+  expose(:cart) { Cart.find_by(user_id: current_user.id) }
 
   def new
-    cart = Cart.find_by(user_id: current_user.id)
     if cart.products.empty?
       redirect_to root_url, notice: "Your cart is empty" and return
     end
@@ -19,6 +18,9 @@ class OrdersController < ApplicationController
     order.user_id = current_user.id
 
     if order.save
+      cart.order_id = order.id
+      cart.save
+      cart.products.destroy_all
       redirect_to root_url, notice: 'Thank you for your order.'
     else
       render :new
